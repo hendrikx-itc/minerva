4f6d47958483ff9184773ca1183aa00683a58e92
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2015-05-08 14:32:13 +0200

    Remove obsolete test code

--- tests/trigger/10_trigger1_testdata.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE VIEW trend."vtransform-retainability-cell_qtr" AS 
-SELECT * FROM (
-	VALUES
-	((directory.dn_to_entity('Cell=4321')).id, '2014-03-06 14:00+01'::timestamp with time zone, 34, 0.99),
-	((directory.dn_to_entity('Cell=4322')).id, '2014-03-06 14:00+01'::timestamp with time zone, 44, 0.94)
-) dummy_values(entity_id, timestamp, "Drops", "CCRSpeech");
-
-ALTER VIEW trend."vtransform-retainability-cell_qtr" OWNER TO minerva_admin;
-GRANT SELECT ON TABLE trend."vtransform-retainability-cell_qtr" TO minerva;
