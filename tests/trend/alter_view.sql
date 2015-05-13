616dc08e329efdc101c2ed912b9c593b11f96424
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2015-05-13 10:25:37 +0200

    Use more appropriate cast to type 'name'

--- tests/trend/alter_view.sql
+++ tests/trend/alter_view.sql
@@ -14,7 +14,7 @@ FROM trend."test-source_test-type_qtr";
 SELECT
     trend_directory.alter_view(view_trend_store, 'SELECT 2 x, 3 y')
 FROM trend_directory.view_trend_store
-WHERE view_trend_store::text = 'test-source_test-type_qtr';
+WHERE view_trend_store::name = 'test-source_test-type_qtr';
 
 SELECT
     is(x, 2)
