12e7891f74cd926b208b9b874646703606d23488
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2015-06-12 15:37:44 +0200

    Add todo view for attribute_to_trend logic

---
+++ scripts/50_storage/trend/50_views.sql
@@ -0,0 +1,14 @@
+CREATE VIEW trend.attribute_to_trend_todo AS
+SELECT foo.processed_modified IS NOT NULL AND foo.processed_modified >= foo.modified AS done, *
+FROM (
+        SELECT
+            s.id attribute_to_trend_id,
+            granularity,
+            trend.get_most_recent_timestamp(s.granularity, now()) AS timestamp,
+            processed_modified,
+            modified
+        FROM trend.attribute_to_trend s
+        LEFT JOIN trend.attribute_to_trend_state att ON att.attribute_to_trend_id = s.id AND att.timestamp = trend.get_most_recent_timestamp(s.granularity, now())
+        LEFT JOIN attribute_directory.attributestore_modified m ON m.attributestore_id = s.attributestore_id
+) foo;
+
