45e0bd21d58389ce79f1fa9d71c00721be674dd7
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2015-06-17 15:26:51 +0200

    Extend tests for define function to include plain relations without view

--- tests/relation/define.sql
+++ tests/relation/define.sql
@@ -1,14 +1,25 @@
 BEGIN;
 
-SELECT plan(2);
+SELECT plan(4);
+
+-- Define plain relation
 
 SELECT relation_directory.define(
-    'A->B',
-    $$SELECT 12 source_id, 22 target_id$$
+    'A->B'
 );
 
 SELECT has_table('relation'::name, 'A->B'::name, 'relation table should exist');
-SELECT has_view('relation_def'::name, 'A->B'::name, 'relation view should exist');
+SELECT hasnt_view('relation_def'::name, 'A->B'::name, 'relation view should not exist');
+
+-- Define relation with view definition
+
+SELECT relation_directory.define(
+    'X->Y',
+    $$SELECT 12 source_id, 22 target_id$$
+);
+
+SELECT has_table('relation'::name, 'X->Y'::name, 'relation table should exist');
+SELECT has_view('relation_def'::name, 'X->Y'::name, 'relation view should exist');
 
 SELECT * FROM finish();
 ROLLBACK;
