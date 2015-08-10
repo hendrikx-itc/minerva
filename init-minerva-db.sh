d02f9e4092bbf2c2b982f6b6cf8c6078ed0bb0b8
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2015-08-10 13:54:49 +0200

    Add default value for 'trigger_entity_tag_denorm_update' setting

--- init-minerva-db.sh
+++ init-minerva-db.sh
@@ -4,3 +4,4 @@ export PGDATABASE=$MINERVA_DB_NAME
 gosu postgres create-minerva-database
 
 echo 'minerva.trigger_mark_modified = on' >> /var/lib/postgresql/data/postgresql.conf
+echo "minerva.trigger_entity_tag_denorm_update = on" >> /var/lib/postgresql/data/postgresql.conf
