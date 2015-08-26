46dfd8ec0bc58b7255d082a3d2386cadbfdfc4ba
Matthijs van der Vleuten <git@zr40.nl>
2015-08-26 16:00:14 +0200

    Use finish time for notification created timestamp

--- scripts/92_trigger/20_functions.sql
+++ scripts/92_trigger/20_functions.sql
@@ -1047,8 +1047,9 @@ DECLARE
 BEGIN
     EXECUTE format(
 $query$
+WITH notifications AS (SELECT entity_id, timestamp, weight, details FROM trigger_rule.%I WHERE timestamp > now() - $2)
 INSERT INTO notification.%I(entity_id, timestamp, created, rule_id, weight, details)
-(SELECT entity_id, timestamp, now(), $1, weight, details FROM trigger_rule.%I WHERE timestamp > now() - $2)
+(SELECT entity_id, timestamp, clock_timestamp(), $1, weight, details FROM notifications)
 $query$,
         notification.staging_table_name($2), trigger.notification_view_name($1)
     )
