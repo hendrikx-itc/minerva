5b77f10cfebb0fc924f1e0a2aa1404ecd23b0844
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2017-08-10 13:35:58 +0200

    Fix compatibility with newer PostgreSQL Docker images

--- init-minerva-db.sh
+++ init-minerva-db.sh
@@ -1,6 +1,6 @@
 export MINERVA_DB_NAME=minerva
 export PGDATABASE=$MINERVA_DB_NAME
 
-gosu postgres create-minerva-database
+create-minerva-database
 
 echo 'minerva.trigger_mark_modified = on' >> /var/lib/postgresql/data/postgresql.conf
