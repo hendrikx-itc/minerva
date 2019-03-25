487c8f5de2ba023953dd93b556f757049fd9e50e
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2019-03-25 15:46:56 +0100

    Init fresh documentation

--- doc/user_guide/create_trigger_kpi_type.sql
+++
@@ -1,5 +0,0 @@
-CREATE TYPE trigger_rule.high_traffic_kpi AS (
-    entity_id integer,
-    timestamp timestamp with time zone,
-    traffic bigint
-);
--- doc/user_guide/create_trigger_set_condition.sql
+++
@@ -1,6 +0,0 @@
-SELECT trigger.set_condition(
-    'high_traffic',
-    $predicate$
-    traffic > max_traffic
-    $predicate$
-);
