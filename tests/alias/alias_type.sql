74160d996c48d0373bbeb87a1e9121a3f42977e8
Andre Engels <andre.engels@hendrikx-itc.nl>
2019-04-08 11:19:54 +0200

    include tests for delete_alias_type

--- tests/alias/alias_type.sql
+++ tests/alias/alias_type.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT plan(6);
+SELECT plan(8);
 
 SELECT bag_eq('SELECT name FROM alias_directory.alias_type', ARRAY['dn'], 'dn should be the only known alias initially');
 
@@ -15,6 +15,13 @@ SELECT set_eq('SELECT name FROM alias_directory.alias_type', ARRAY['dn', 'sample
 SELECT bag_eq($$ SELECT name FROM alias_directory.get_alias_type('sample') $$, ARRAY['sample'], 'alias should be found when defined');
 
 SELECT throws_like($$ SELECT alias_directory.create_alias_type('sample') $$, '%duplicate key value%', 'creating an alias type twice should not be possible');
+
+SELECT alias_directory.delete_alias_type(alias_directory.get_alias_type('sample'));
+
+SELECT bag_eq('SELECT name FROM alias_directory.alias_type', ARRAY['dn'], 'deleting alias type should remove it from the directory');
+
+SELECT tables_are('alias', ARRAY['dn'], 'alias table should be deleted when alias type is deleted');
+
 
 SELECT * FROM finish();
 ROLLBACK;
