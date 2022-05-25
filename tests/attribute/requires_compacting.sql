f4a6cfae7d333054c4959de2e02c0b343839b2b9
Andre Engels <andre.engels@hendrikx-itc.nl>
2022-05-25 14:05:49 +0200

    update requires compacting test

--- tests/attribute/requires_compacting.sql
+++ tests/attribute/requires_compacting.sql
@@ -2,50 +2,49 @@ BEGIN;
 
 SELECT plan(4);
 
-INSERT INTO directory.data_source (id, name, description) VALUES
-    (1, 'ds', 'some datasource');
+CALL attribute_directory.create_attribute_store('ds', 'type1');
+CALL attribute_directory.create_attribute_store('ds', 'type2');
+CALL attribute_directory.create_attribute_store('ds', 'type3');
+CALL attribute_directory.create_attribute_store('ds', 'type4');
 
-INSERT INTO directory.entity_type (id, name, description) VALUES
-    (1, 'type1', 'some type'),
-    (2, 'type2', 'some type'),
-    (3, 'type3', 'some type'),
-    (4, 'type4', 'some type');
+INSERT INTO attribute_history.ds_type1(entity_id, timestamp) VALUES
+    ((entity.to_type1('entity1')).id, now());
+INSERT INTO attribute_history.ds_type1(entity_id, timestamp) VALUES
+    ((entity.to_type1('entity2')).id, now());
+INSERT INTO attribute_history.ds_type1(entity_id, timestamp) VALUES
+    ((entity.to_type1('entity3')).id, now());
 
-INSERT INTO attribute_directory.attribute_store (id, data_source_id, entity_type_id) VALUES
-    (1, 1, 1),
-    (2, 1, 2),
-    (3, 1, 3),
-    (4, 1, 4);
+SELECT attribute_directory.compact(attribute_directory.get_attribute_store('ds', 'type1'), 0);
+SELECT attribute_directory.compact(attribute_directory.get_attribute_store('ds', 'type2'), 0);
 
-INSERT INTO attribute_directory.attribute_store_modified (attribute_store_id, modified) VALUES
-    (1, '2018-01-01 00:00:00'),
-    (2, '2018-01-01 00:00:00'),
-    (3, '2018-01-01 00:00:00');
 
-INSERT INTO attribute_directory.attribute_store_compacted (attribute_store_id, compacted) VALUES
-    (1, '2018-01-01 00:00:00'),
-    (2, '2017-01-01 00:00:00');
+INSERT INTO attribute_history.ds_type1(entity_id, timestamp) VALUES
+    ((entity.to_type1('entity1')).id, now());
+INSERT INTO attribute_history.ds_type1(entity_id, timestamp) VALUES
+    ((entity.to_type1('entity2')).id, now());
+
+SELECT attribute_directory.compact(attribute_directory.get_attribute_store('ds', 'type1'), 0);
 
 SELECT is(
-    attribute_directory.requires_compacting(1),
+    attribute_directory.requires_compacting(attribute_directory.get_attribute_store('ds', 'type1')),
     false,
     'recently compacted attribute_store should not need compacting'
 );
 
 SELECT is(
-    attribute_directory.requires_compacting(2),
+    attribute_directory.requires_compacting(attribute_directory.get_attribute_store('ds', 'type2')),
     true,
     'attribute_store that is modified after compacting should need compacting'
 );
 
 SELECT is(
-    attribute_directory.requires_compacting(3),
+    attribute_directory.requires_compacting(attribute_directory.get_attribute_store('ds', 'type3')),
     true,
     'attribute_store that was never compacted should need compacting'
 );
 
 SELECT isnt(
-    attribute_directory.requires_compacting(4),
+    attribute_directory.requires_compacting(attribute_directory.get_attribute_store('ds', 'type4')),
     true,
     'attribute_store that was never modified should not need compacting'
 );
