b831fde9a908f09a74b057f1ded3120b8682ad2f
Alfred Blokland <alfred.blokland@1optic.io>
2024-04-24 11:57:22 +0200

    Show help if no command is given

--- crates/cli/main.rs
+++ crates/cli/main.rs
@@ -18,7 +18,7 @@ use crate::commands::update::UpdateOpt;
 use crate::commands::relation::RelationOpt;
 
 #[derive(Parser, Debug, PartialEq)]
-#[command(name = "minerva")]
+#[command(version, about, name = "minerva", arg_required_else_help = true)]
 struct Cli {
     #[arg(long = "generate", value_enum)]
     generator: Option<Shell>,
