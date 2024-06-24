8b3c84b0ccc4bb9427f719198c9c5ab1aa671cbd
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2024-06-25 00:45:42 +0200

    WIP

--- crates/integration-tests/src/get_entity_types.rs
+++ crates/integration-tests/src/get_entity_types.rs
@@ -8,22 +8,20 @@ mod tests {
     use std::net::IpAddr;
     use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4, TcpListener, TcpStream};
     use std::process::Command;
-    use chrono::DateTime;
-    use std::str::FromStr;
 
     use rand::distributions::{Alphanumeric, DistString};
 
     use tokio_postgres::config::Config;
     use tokio_postgres::{Client, NoTls};
+    use tokio::io::AsyncBufReadExt;
     use tokio::time::{sleep, Duration};
 
-    use testcontainers::core::{ContainerPort, ContainerRequest, WaitFor};
+    use testcontainers::core::{ContainerPort, ContainerRequest, WaitFor, Mount};
     use testcontainers::{GenericImage, ImageExt};
     use testcontainers_modules::testcontainers::runners::AsyncRunner;
 
     use minerva::change::Change;
-    use minerva::database::{connect_to_db, create_database, drop_database, get_db_config};
-    use minerva::instance::MinervaInstance;
+    use minerva::database::{create_database, drop_database};
     use minerva::changes::trend_store::AddTrendStore;
     use minerva::schema::create_schema;
     use minerva::trend_store::{create_partitions_for_timestamp, TrendStore};
@@ -57,29 +55,28 @@ mod tests {
         Alphanumeric.sample_string(&mut rand::thread_rng(), 16)
     }
 
-    fn create_citus_controller() -> ContainerRequest<GenericImage> {
+    fn create_citus_container(name: &str, exposed_port: Option<u16>) -> ContainerRequest<GenericImage> {
         let image = GenericImage::new(CITUS_IMAGE, CITUS_TAG)
-            .with_exposed_port(ContainerPort::Tcp(5432))
             .with_wait_for(WaitFor::message_on_stdout(
                 "PostgreSQL init process complete; ready for start up.",
             ));
 
-        let request = ContainerRequest::from(image);
-
-        request.with_env_var("POSTGRES_HOST_AUTH_METHOD", "trust")
-    }
-
-    fn create_citus_node() -> ContainerRequest<GenericImage> {
-        let image = GenericImage::new(CITUS_IMAGE, CITUS_TAG)
-            .with_exposed_port(ContainerPort::Tcp(5432))
-            .with_wait_for(WaitFor::message_on_stdout(
-                "PostgreSQL init process complete; ready for start up.",
-            ));
+        let image = match exposed_port {
+            Some(port) => image.with_exposed_port(ContainerPort::Tcp(port)),
+            None => image
+        };
 
         let request = ContainerRequest::from(image);
 
-        request.with_env_var("POSTGRES_HOST_AUTH_METHOD", "trust")
+        let conf_file_path = concat!(env!("CARGO_MANIFEST_DIR"), "/postgresql.conf");
+
+        request
+            .with_env_var("POSTGRES_HOST_AUTH_METHOD", "trust")
+            .with_container_name(name)
+            .with_mount(Mount::bind_mount(conf_file_path, "/etc/postgresql/postgresql.conf"))
+            .with_cmd(vec!["-c", "config-file=/etc/postgresql/postgresql.conf"])
     }
+
     async fn connect_db(host: url::Host, port: u16) -> Client {
         let mut config = Config::new();
 
@@ -116,7 +113,6 @@ mod tests {
     }
 
     async fn add_worker(client: &mut Client, host: IpAddr, port: u16) -> Result<(), String> {
-        println!("Host: {host:?}");
         let _count = client
             .execute(
                 "SELECT citus_add_node($1, $2)",
@@ -128,11 +124,27 @@ mod tests {
         Ok(())
     }
 
+    fn print_stdout<I: tokio::io::AsyncBufRead + std::marker::Unpin + std::marker::Send + 'static>(prefix: String, mut reader: I) {
+        tokio::spawn(async move {
+            let mut buffer = String::new();
+            loop {
+                let result = reader.read_line(&mut buffer).await;
+
+                if let Ok(0) = result { break };
+
+                print!("{prefix} - {buffer}");
+
+                buffer.clear();
+            }
+        });
+    }
+
     #[cfg(test)]
     #[tokio::test]
     async fn get_entity_types() -> Result<(), Box<dyn std::error::Error>> {
-        let network = "test".to_string();
-        let controller_container = create_citus_controller()
+        env_logger::init();
+        let network = "test2".to_string();
+        let controller_container = create_citus_container("coordinator", Some(5432))
             .with_network(network.clone())
             .start()
             .await
@@ -150,35 +162,15 @@ mod tests {
         println!("Connecting to controller");
         let mut client = connect_db(controller_host.clone(), controller_port).await;
         println!("Creating Minerva schema");
-        create_schema(&mut client).await.unwrap();
+        //create_schema(&mut client).await.unwrap();
         println!("Initializing");
         let instance_path = Path::new(concat!(
             env!("CARGO_MANIFEST_DIR"),
             "/dev-stack/minerva-instance"
         ));
-        let minerva_instance = MinervaInstance::load_from(instance_path);
-        minerva_instance.initialize(&mut client).await;
+        //let minerva_instance = MinervaInstance::load_from(instance_path);
+        //minerva_instance.initialize(&mut client).await;
         println!("Initialized");
-
-        let node1_container = create_citus_node()
-            .with_network(network.clone())
-            .start()
-            .await
-            .unwrap();
-        let node2_container = create_citus_node()
-            .with_network(network.clone())
-            .start()
-            .await
-            .unwrap();
-        let node3_container = create_citus_node()
-            .with_network(network.clone())
-            .start()
-            .await
-            .unwrap();
-
-        println!("Containers started");
-
-        println!("Connecting nodes");
 
         let coordinator_host = controller_container
             .get_bridge_ip_address()
@@ -198,52 +190,45 @@ mod tests {
             .await
             .unwrap();
 
-        add_worker(
-            &mut client,
-            node1_container.get_bridge_ip_address().await.unwrap(),
-            5432,
-        )
-        .await
-        .expect("Could not connect node");
-        add_worker(
-            &mut client,
-            node2_container.get_bridge_ip_address().await.unwrap(),
-            5432,
-        )
-        .await
-        .expect("Could not connect node");
-        add_worker(
-            &mut client,
-            node3_container.get_bridge_ip_address().await.unwrap(),
-            5432,
-        )
-        .await
-        .expect("Could not connect node");
+        let num_workers = 3;
+        let mut node_containers = Vec::new();
 
-        println!("Connected nodes");
+        for i in 1..(num_workers + 1) {
+            let name = format!("node{i}");
+            let container = create_citus_container(&name, None)
+                .with_network(network.clone())
+                .start()
+                .await
+                .unwrap();
 
-        println!("Creating partitions");
-        let timestamp = DateTime::from_str("2023-09-20T22:15:00+02:00").unwrap();
-        create_partitions_for_timestamp(&mut client, timestamp)
+            print_stdout(name, container.stdout(true));
+
+            let container_address = container.get_bridge_ip_address().await.unwrap();
+
+            println!("Adding worker {container_address}");
+
+            sleep(Duration::from_secs(3)).await;
+
+            add_worker(
+                &mut client,
+                container_address,
+                5432,
+            )
             .await
-            .unwrap();
-        println!("Created partitions");
+            .expect("Could not connect worker");
 
+            node_containers.push(Box::pin(container));
+        }
 
-
-
-
+        println!("Containers started");
 
 
         let database_name = generate_name();
-        let db_config = get_db_config()?;
-        let mut client = connect_to_db(&db_config).await?;
 
         create_database(&mut client, &database_name).await?;
         println!("Created database '{database_name}'");
 
         {
-            let mut client = connect_to_db(&db_config.clone().dbname(&database_name)).await?;
             create_schema(&mut client).await?;
 
             let trend_store: TrendStore = serde_yaml::from_str(TREND_STORE_DEFINITION)
@@ -298,7 +283,7 @@ mod tests {
             Ok(_) => println!("Stopped web service"),
         }
 
-        let mut client = connect_to_db(&db_config).await?;
+        //let mut client = connect_to_db(&db_config).await?;
 
         drop_database(&mut client, &database_name).await?;
 
